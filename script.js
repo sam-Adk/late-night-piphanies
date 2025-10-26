50780d1e34d936465113446403df9edb719cc9ba
Sam-Adk <samsmollett50@gmail.com>
2025-10-26 15:08:29 +0300

    Initial commit - Late Night Piphanies project

---
+++ script.js
@@ -0,0 +1,22 @@
+const products = [
+  { id: 'p1', title: 'Love Dream I', category: 'Portrait', tags:['romance','bw'], priceBase: 4200,
+    img: 'assets/images/love.jpg',
+    desc: 'Romantic midnight sketch in pencil.'
+  },
+  { id: 'p2', title: 'Love Dream II', category: 'Sketch', tags:['dreamy','soft'], priceBase: 2500,
+    img: 'assets/images/love1.png',
+    desc: 'A dreamy late-night drawing, soft and textured.'
+  },
+  { id: 'p3', title: 'Love Dream III', category: 'Abstract', tags:['surreal','thoughts'], priceBase: 5200,
+    img: 'assets/images/love2.jpg',
+    desc: 'Surreal abstraction inspired by late-night epiphanies.'
+  },
+  { id: 'p4', title: 'Love Dream IV', category: 'Sketch', tags:['raw','expressive'], priceBase: 3000,
+    img: 'assets/images/love3.jpg',
+    desc: 'Expressive raw strokes, part of the Love series.'
+  },
+   { id: 'p4', title: 'Love Dream IV', category: 'Sketch', tags:['raw','expressive'], priceBase: 3000,
+    img: 'assets/images/love3.jpg',
+    desc: 'Expressive raw strokes, part of the Love series.'
+  }
+];
